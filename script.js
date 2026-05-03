f494555a54d2d11ecc6e0cb09a88289b6654c02c
Nirjhar Bhattacharjee <bhattacharjeenirjhar26@gmail.com>
2026-05-03 21:55:33 +1000

    take input and display it

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 const display = document.querySelector('.display');
+const decimal = document.querySelector('#dot');
 const numberButtons = document.querySelectorAll('.numbers');
-
+const operatorButtons = document.querySelectorAll('operator');
+const clear = document.querySelector('#clear');
+const allClear = document.querySelector('#all-clear');
 
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const division = (num1, num2) => num1 / num2;
 
-numberButtons.forEach( numberButton => {
-    numberButton.addEventListener("click", (e) => {
-    let numValue = e.target.value;
-    display.textContent = numValue;
-    console.log(numValue);
+
+// this function operates with 2 operands 
+function operate(operator, operand1, operand2){
+    if (operator === '+') {
+        return add(operand1,operand2);
+    }
+    else if (operator === '-') {
+        return subtract(operand1,operand2 );
+    } 
+    else if (operator === '*') {
+        return multiply(operand1,operand2);
+    }
+    else if(operator ==='/'){
+        return division(operand1,operand2)
+    }
+}
+
+let prevNumber="";
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", (e)=>{
+        const number = e.target.value;
+        prevNumber += number;
+        display.innerText = prevNumber;        
     });
 });
+
+
+
+    
+
+
+
